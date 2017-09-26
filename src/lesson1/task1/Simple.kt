467aabdb79af3956af48aa3379e7aefaa96b9953
Violetta-Mamykina <violetta-1998m@mail.ru>
2017-09-26 19:22:17 +0300

    реализовала первый урок

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -61,7 +61,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours*3600+minutes*60
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
 
-    fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes * 48 + arshins * 16 + vershoks) * 4.445 / 100
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+        (sagenes * 48 + arshins * 16 + vershoks) * 4.445 / 100
 
 /**
  * Тривиальная
@@ -69,7 +70,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours*3600+minutes*60
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad*3600+min*60+sec)*PI/648000
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad*60*60+min*60+sec)*PI/180*60*60
 
 /**
  * Тривиальная
@@ -94,7 +95,8 @@ fun thirdDigit(number: Int): Int = (number/100)%10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = (hoursArrive*60+minutesArrive)-(hoursDepart*60+minutesDepart)
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+        (hoursArrive*60+minutesArrive)-(hoursDepart*60+minutesDepart)
 
 /**
  * Простая
@@ -103,7 +105,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = initial*pow(1.0+percent/100, 3.0)
+fun accountInThreeYears(initial: Int, percent: Int): Double = initial*pow(1.0+percent*0.01, 3.0)
 
 /**
  * Простая
