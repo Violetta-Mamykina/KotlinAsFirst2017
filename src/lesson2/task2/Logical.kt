ad5d3eea272c07e58ee3404e0696c762298bcba8
Violetta-Mamykina <violetta-1998m@mail.ru>
2017-10-01 15:35:44 +0300

    реализовала урок Logical

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -2,6 +2,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import java.lang.Math.sqrt
 
 /**
  * Пример
@@ -17,7 +18,12 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val sum12 = ((number/1000) + (number/100)%10)
+    val sum34 = (((number/10)%10) + (number%10))
+    return (sum12 == sum34)
+
+}
 
 /**
  * Простая
@@ -26,7 +32,11 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    val distanceX = Math.abs(x2-x1)
+    val destanceY = Math.abs(y2-y1)
+    return ((distanceX == destanceY)|| (x1 == x2) || (y1 == y2))
+}
 
 /**
  * Средняя
@@ -36,7 +46,10 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    val distance = sqrt(sqr(x2-x1)+sqr(y2-y1))
+    return (r2 >= distance + r1)
+}
 
 /**
  * Средняя
@@ -47,4 +60,10 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    return ((b>=a && b>=c) && ((r>=a && s>=c) || (r>=c && s>=a)) || (c>=a && c>=b) && ((r>=b && s>=a) || (r>=a && s>=b))
+            || ((a>=b && a>=c) && (r>=b && s>=c) || (r>=c && s>=b)))
+
+    }
+
+
